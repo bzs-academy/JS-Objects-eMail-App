9ca6166b06e89eb3fde4524df9ae77ff4b7754ee
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2020-01-11 10:38:45 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,88 @@
+let emails = [];
+
+function sendEmail() {
+    let to = document.getElementById('to').value;
+    let cc = $('#cc').val();
+    let subject = $('#subject').val();
+    let message = $('#text').val();
+    let email = {to, cc, subject, message};
+    
+    emails.push(email);
+    console.log(emails);
+    localStorage.setItem('emails', JSON.stringify(emails));
+
+}
+/* 
+function showInbox() {
+    document.getElementById('inboxLink').classList.add('active');
+    document.getElementById('composeLink').classList.remove('active');
+
+    inbox.hidden = false;
+    compose.hidden = true;
+}
+
+function showCompose() {
+    inboxLink.classList.remove('active');
+    composeLink.classList.add('active');
+
+    inbox.hidden = true;
+    compose.hidden = false;
+} */
+
+function switchScreen(reqScreen) {
+    if (reqScreen == 'inbox') {
+        inboxLink.classList.add('active');
+        composeLink.classList.remove('active');
+    
+        inbox.hidden = false;
+        compose.hidden = true;
+        //reqScreen = 'compose';
+        getEmails();
+    }else if(reqScreen == 'compose'){
+        document.getElementById('inboxLink').classList.remove('active');
+        document.getElementById('composeLink').classList.add('active');
+    
+        inbox.hidden = true;
+        compose.hidden = false;
+        //reqScreen = 'inbox';
+    }else{
+        alert("There's a problem. Please contact with support service");
+    }
+}
+
+function getEmails() {
+    //<button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+    mailList.innerHTML = "";
+
+    let mailBox = JSON.parse(localStorage.getItem('emails'));
+    mailBox.map(val=>{
+        $('#mailList').append(`<button type="button" class="list-group-item list-group-item-action">${val.subject}</button>`);
+    });
+
+}
+
+getEmails();
+
+/*  
+let mailArr = [
+    {
+        to: "bulent.kayici@bbb.com", 
+        cc: "dddd", 
+        subject: "wtreet", 
+        message: "dsfsdfddd"
+    },
+    {
+        to: "bulent.kayici@bbb.com", 
+        cc: "dddd", 
+        subject: "wtreet", 
+        message: "dsfsdfddd"
+    },
+    {
+        to: "bulent.kayici@bbb.com", 
+        cc: "dddd", 
+        subject: "wtreet", 
+        message: "dsfsdfddd"
+    }
+]
+
+*/
